28ce691b079fb8cd785a53370e5f36ed73ad419f
feomedeirosdev <19139442-feomedeirosdev@users.noreply.replit.com>
2023-06-12 00:17:20 +0000

    Iniciando Repositório

---
+++ 002_Respondendo_ao_usuario.py
@@ -0,0 +1,2 @@
+nome = input(str("\nQual é o seu nome? \n"))
+print(f"\nOlá {nome}! Seja bem vindo!\n")
---
+++ 003_Somando_dois_numeros.py
@@ -0,0 +1,4 @@
+n1 = float(input("Digite o 1º número: "))
+n2 = float(input("Digite o 2º número: "))
+print(f"A soma enter {n1} e {n2} é: {n1+n2}")
+print(f"{n1} + {n2} = {n1+n2}")

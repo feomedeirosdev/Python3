aee1ab412962ec7ef36452341b9831f96b932ed9
feomedeirosdev <120518007+feomedeirosdev@users.noreply.github.com>
2023-06-12 01:11:25 +0000

    ex006

---
+++ 005_Antecessor_e_sucessor.py
@@ -0,0 +1,2 @@
+n = int(input("\nDigite um número inteiro qualquer: \n"))
+print(f"\nAnalizando o número {n}, seu antecessor é {n-1} e seu sucessor é {n+1}.\n")
---
+++ 006_Dobro_Triplo_Raiz.py
@@ -0,0 +1,4 @@
+n = float(input("\nEntre com um número qualquer: "))
+print(f"O dobro de {n} é: {2*n}")
+print(f"O troplo de {n} é: {3*n}")
+print(f"E sua raiz quadrada é: {n**(1/2)}")
